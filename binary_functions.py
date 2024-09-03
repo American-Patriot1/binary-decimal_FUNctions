286413e33d08ba32372b50cff0a4b8025c22a129
Nathaniel Havens <97206671+American-Patriot1@users.noreply.github.com>
2024-09-03 21:29:24 +0000

    Finished

--- binary_functions.py
+++ binary_functions.py
@@ -1,1 +1,21 @@
-
+numm=255
+def bin_to_num(num):
+    rnum=0
+    curant_num=1
+    for i in range(len(num)):
+        if (num[((i+1)*-1)])=="1":
+            rnum+=curant_num
+        curant_num=curant_num*2
+    return rnum
+def num_to_bin(num):
+    cnum=128
+    rnum=""
+    for i in range(8):
+        if num-cnum>=0:
+            num=num-cnum
+            rnum=rnum+"1"
+        else:
+            rnum=rnum+"0"
+        cnum=cnum/2
+    return(rnum)
+print(num_to_bin(numm))
